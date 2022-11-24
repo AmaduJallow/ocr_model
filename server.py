e28d2de90b0a2361cdc4c497bbe9c122b975f228
Amadu Jallow <aj22011021@utg.edu.gm>
2022-11-24 13:17:24 +0530

    Riama

--- server.py
+++ server.py
@@ -1,23 +1,43 @@
 from fastapi import FastAPI
 import pytesseract
 import PIL.Image
+import cv2
 from starlette.middleware.cors import CORSMiddleware
 import urllib.request
+import numpy as np
 
 # import pillow
 from model import Model
 
 
+def get_grayscale(image):
+    return cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+
+def remove_noise(image):
+    return cv2.medianBlur(image, 5)
+
+
+def thresholding(image):
+    return cv2.threshold(image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+
+
 def imagegetter(url):
+    text = None
     myconfig = r"--psm 6 --oem 1"
     if url.__contains__("http://") or url.__contains__("https://"):
         urllib.request.urlretrieve(url, "placeholder.jpg")
-        text = pytesseract.image_to_string(
-            PIL.Image.open("placeholder.jpg"), config=myconfig)
+        img2 = np.array(PIL.Image.open("placeholder.jpg"))
+        img2 = get_grayscale(img2)
+        img2 = remove_noise(img2)
+        img2 = thresholding(img2)
+        text = pytesseract.image_to_string(img2)
     else:
-        text = pytesseract.image_to_string(
-            PIL.Image.open(url), config=myconfig)
-
+        img2 = np.array(PIL.Image.open(url))
+        img2 = get_grayscale(img2)
+        img2  = remove_noise(img2)
+        img2 = thresholding(img2)
+        text = pytesseract.image_to_string(img2)
     return text
 
 
@@ -44,4 +64,4 @@ async def imager(data: Model):
     return text_data
 
 
-print(imagegetter('./image_4.png'))
+print(imagegetter('./everything-has-beauty-confucius-quote.jpg'))
